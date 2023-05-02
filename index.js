77d8a595ca9ca220e524181d9a0429b64499dd51
Shahidul Islam <shahidulislam@Konas-iMac.local>
2023-05-02 22:33:51 +0600

    test three

--- index.js
+++ index.js
@@ -1,31 +1,48 @@
+// const express = require('express');
+// const app = express();
+// const cors = require('cors')
+// const port =process.env.PORT || 8000 ;
+// app.use(cors())
+
+// const datas = require('./Data/data.json')
+
+// app.get('/', (req, res) => {
+//     res.send('Hello eveyone');
+// })
+// app.get('/datas',(req, res)=>{
+//     res.send(datas)
+// })
+
+// app.get('/recipes/:id', (req,res)=>{
+//     const id = req.params.id;
+//     const singleData = datas.find(d => d.id == id)
+//     res.send(singleData)
+// })
+
+
+// app.get('/data', (req, res)=>{
+//    res.send('Hi everuone')
+// })
+
+
+
+// app.listen(port, ()=>{
+// console.log(`Server is running in port ${port}`)
+// })
+
 const express = require('express');
 const app = express();
-const cors = require('cors')
-const port =process.env.PORT || 8000 ;
-app.use(cors())
-
-const datas = require('./Data/data.json')
-
-app.get('/', (req, res) => {
-    res.send('Hello eveyone');
-})
-app.get('/datas',(req, res)=>{
-    res.send(datas)
-})
-
-app.get('/recipes/:id', (req,res)=>{
-    const id = req.params.id;
-    const singleData = datas.find(d => d.id == id)
-    res.send(singleData)
-})
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 
 
-app.get('/data', (req, res)=>{
-   res.send('Hi everuone')
-})
+app.use(cors());
+
+app.get('/', (req, res) =>{
+    res.send('Dragon is running')
+});
 
 
-
-app.listen(port, ()=>{
-console.log(`Server is running in port ${port}`)
+app.listen(port, () => {
+    console.log(`Dragon API is running on port: ${port}`)
 })

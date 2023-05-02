0810f0fc5b641c4008c030938141254a519db976
Shahidul Islam <shahidulislam@Konas-iMac.local>
2023-05-02 21:49:23 +0600

    test file

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const port =process.env.PORT || 5000 ;
+app.use(cors())
+
+const datas = require('./Data/data.json')
+
+app.get('/', (req, res) => {
+    res.send('Hello everyone');
+})
+app.get('/datas',(req, res)=>{
+    res.send(datas)
+})
+
+app.get('/recipes/:id', (req,res)=>{
+    const id = req.params.id;
+    const singleData = datas.find(d => d.id == id)
+    res.send(singleData)
+})
+
+
+app.get('/data', (req, res)=>{
+   res.send('Hi everuone')
+})
+
+
+
+app.listen(port, ()=>{
+console.log(`Server is running in port ${port}`)
+})
